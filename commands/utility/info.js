2fc3144735a3c4ffbfc0268eb3f290e1aa5a22a7
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-02-03 19:54:35 -0300

    Adiciona sessão de server infos

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const package = require("../../package.json");
 const { cpu, mem, os } = require("node-os-utils");
 const uptimeBot = require("pretty-ms")
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js')
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ChannelType } = require('discord.js')
 
 module.exports = {
   name: 'info',
@@ -12,17 +12,25 @@ module.exports = {
   run: async (client, interaction) => {
 
     const operatingSystem = `${os.type()} ${await os.oos().then(o => o)} ${os.arch()}`
-    const cpuUsage = (await cpu.usage()) + "%"
+    const cpuUsage = (await cpu.usage()).toFixed(2) + "%"
     const memoryUsage = (await mem.info()).usedMemMb;
     const memoryTotal = (await mem.info()).totalMemMb;
+    const memoryPorcentage = ((memoryUsage / memoryTotal) * 100).toFixed(0) + '%'
 
     const row = new ActionRowBuilder()
       .addComponents(
         new ButtonBuilder()
           .setCustomId('primary')
-          .setLabel('Info Server')
+          .setLabel('Server Info')
           .setStyle(ButtonStyle.Primary)
           .setEmoji('<:MH_Info_Pink:1060401054379945994>'),
+      )
+      .addComponents(
+        new ButtonBuilder()
+          .setLabel('Github Project')
+          .setStyle(ButtonStyle.Link)
+          .setURL('https://github.com/Actunes/Spartacus-Bot')
+          .setEmoji('<:github71:1071094653060923533>'),
       )
 
     let embedInfo = new Discord.EmbedBuilder()
@@ -30,7 +38,7 @@ module.exports = {
       .setTitle('About Spartacus Bot')
       .setDescription('Spartacus bot was created and thought about the needs that the spartacus group needs for its development.\n\nTo start using the bot you can type `/` and select from the available commands.\n\n<:system77:1060391259153375332> System Monitor')
       .addFields(
-        { name: `**RAM**`, value: '```' + Math.trunc(memoryUsage) + ' MB' + ' | ' + Math.trunc(memoryTotal) + ' MB' + '```', inline: true },
+        { name: `**RAM**`, value: '```' + Math.trunc(memoryUsage) + ' MB' + ' | ' + Math.trunc(memoryTotal) + ' MB ' + `(${memoryPorcentage})` + '```', inline: true },
         { name: `**CPU_USAGE**`, value: '```' + `${cpuUsage}` + '```', inline: true },
         { name: `**PING**`, value: '```' + `${Math.round(client.ws.ping)}ms` + '```', inline: true },
         { name: `**OS**`, value: '```' + operatingSystem + '```', inline: true },
@@ -38,6 +46,54 @@ module.exports = {
         { name: `**DISCORD.JS**`, value: '```' + `${package.dependencies["discord.js"].slice(1)}` + '```', inline: true },
         { name: `**UPTIME**`, value: '```' + `${uptimeBot(client.uptime, { verbose: true })}` + '```', inline: true }
       )
-    interaction.reply({ embeds: [embedInfo], ephemeral: true, components: [row] })
+    await interaction.reply({ embeds: [embedInfo], ephemeral: true, components: [row] })
+
+    const guildName = interaction.guild.name
+    const getChannelTypeSize = type => channel.cache.filter(channel => type.includes(channel.type)).size
+    await interaction.guild.members.fetch()
+    const botCount = interaction.guild.members.cache.filter(member => member.user.bot).size
+
+    let embedServer = new Discord.EmbedBuilder()
+      .setColor('#700000')
+      .setTitle(guildName)
+      .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
+      .setDescription('Informations about current server')
+      .addFields(
+        {
+          name: 'General', value: [
+            `** :calendar_spiral: Created at** <t:${parseInt(interaction.guild.createdTimestamp / 1000)}:R>`,
+            `** :identification_card: ID** ${interaction.guild.id}`,
+            `** :crown: Owner** <@${interaction.guild.ownerId}>`,
+            `**:earth_americas: Language** ${new Intl.DisplayNames(["pt-BR"], { type: "language" }).of(interaction.guild.preferredLocale)}`
+          ].join('\n')
+        },
+        {
+          name: `Channels, threads and categories (${interaction.guild.channels.cache.size})`, value: [
+            `**Categories** ${interaction.guild.channels.cache.filter(c => c.type === Discord.ChannelType.GuildCategory).size}`,
+            `**Text channels** ${interaction.guild.channels.cache.filter(c => c.type === Discord.ChannelType.GuildText).size}`,
+            `**Voice channels** ${interaction.guild.channels.cache.filter(c => c.type === Discord.ChannelType.GuildVoice).size}`,
+          ].join('\n'), inline: true
+        },
+        {
+          name: `Emojis and Stickers (${interaction.guild.emojis.cache.size + interaction.guild.stickers.cache.size})`, value: [
+            `**Animated** ${interaction.guild.emojis.cache.filter(emoji => emoji.animated).size}`,
+            `**Static** ${interaction.guild.emojis.cache.filter(emoji => !emoji.animated).size}`,
+            `**Sticker** ${interaction.guild.stickers.cache.size}`
+          ].join('\n'), inline: true
+        },
+        {
+          name: `Members (${interaction.guild.memberCount})`, value: [
+            `**Users** ${interaction.guild.memberCount - botCount}`,
+            `**Bots** ${botCount}`
+          ].join('\n'), inline: true
+        }
+
+      )
+
+    client.once(Events.InteractionCreate, interaction => {
+      if (!interaction.isButton()) return
+      interaction.reply({ embeds: [embedServer], ephemeral: true })
+    })
+
   }
 }
