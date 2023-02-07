d4be9f7517848ee0ea3f8c355bb724ded167f760
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-02-07 09:24:40 -0300

    Melhora channelReact

--- events/channelReact.js
+++ events/channelReact.js
@@ -1,14 +1,7 @@
 const client = require("..")
 
-client.on("messageCreate", (message) => {
-    if (message.channel.id === "") {
+client.on('messageCreate', message => {
+    if (!['1058745163784142910', '1062861256685531146', '1058393979684667412'].includes(message.channel.id) || message.author.bot) return;
 
-        let emoji_accept = "✅"
-        let emoji_rejected = "❌"
-
-        message.react(emoji_accept).catch(e => { })
-        message.react(emoji_rejected).catch(e => { })
-
-    } else { return; }
+    ['<:check:1072492827285667910>', '<:deny:1072492829735133275>'].map(emj => message.react(emj).catch(e => null))
 })
-
