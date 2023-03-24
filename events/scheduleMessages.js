3d6b23b45dd9172b2fd1ca4c8bca1e3349e39db0
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-03-24 16:10:01 -0300

    🐛 Corrige loop indesejado

--- events/scheduleMessages.js
+++ events/scheduleMessages.js
@@ -8,17 +8,17 @@ client.once("ready", () => {
     const monthNames = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
     const month = monthNames[date.getMonth()]
 
-    const scheduledMessage1 = new cron.CronJob('* * * 25 * *', async () => {
+    const scheduledMessage1 = new cron.CronJob('0 0 3 25 * *', async () => {
         const guild = client.guilds.cache.get(guildID)
         const channel = guild.channels.cache.get(channelID)
-        const msg = await channel.send({ content: `📢 Esta é a Chamada Obrigatória do mês de **${month}**. Todos os <@&1075618605439398038> tem 7 dias para confirmar na reação abaixo que estão ativos. Caso contrário, podem receber 2 pontos de infração.\n\n⚠️ Atenção a quem está em <@&1075618607662387210>, teremos uma chamada **especifica** para vocês.` })
+        const msg = await channel.send({ content: `📢 Esta é a Chamada Obrigatória do mês de **${month}**. Todos os <@&1020194745722609684> tem 7 dias para confirmar na reação abaixo que estão ativos. Caso contrário, podem receber 2 pontos de infração.\n\n⚠️ Atenção a quem está em <@&1020234436236816394>, teremos uma chamada **especifica** para vocês.` })
         msg.react(`<:spts:863119583275384864>`)
     })
     scheduledMessage1.start()
-    const scheduledMessage2 = new cron.CronJob('* * * 27 * *', async () => {
+    const scheduledMessage2 = new cron.CronJob('0 0 3 27 * *', async () => {
         const guild = client.guilds.cache.get(guildID)
         const channel = guild.channels.cache.get(channelID)
-        const msg = await channel.send({ content: `⚠️ Atenção membros em <@&1075618607662387210> marcados nessa mensagem.\n\nVocê tem 5 dias para reagir no ✅ abaixo. Caso contrário, será considerado inativo e perderá o cargo de membro.` })
+        const msg = await channel.send({ content: `⚠️ Atenção membros em <@&1020234436236816394> marcados nessa mensagem.\n\nVocê tem 5 dias para reagir no ✅ abaixo. Caso contrário, será considerado inativo e perderá o cargo de membro.` })
         msg.react(`✅`)
     })
     scheduledMessage2.start()
